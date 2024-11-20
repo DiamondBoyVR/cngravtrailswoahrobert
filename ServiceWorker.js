3e0367f3b8be28c023cffa673ae4e1c2d97d686d
DiamondBoyVR <Diamondboycoder@gmail.com>
2024-11-20 17:32:43 -0500

    WORKTHIS TIME
    peleasE?

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-CN-GravityTrails-0.1";
+const contentToCache = [
+    "Build/gravtrailswebglnocompression.loader.js",
+    "Build/gravtrailswebglnocompression.framework.js",
+    "Build/gravtrailswebglnocompression.data",
+    "Build/gravtrailswebglnocompression.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
